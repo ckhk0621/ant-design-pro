8624152f49cb64821759f0f91744cc8133b13102
CK Lam <ckhk0621@gmail.com>
2019-04-07 16:56:08 +0800

    [WIP]Upload images

---
+++ src/components/UploadImage/index.d.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+import { DropDownProps } from 'antd/lib/dropdown';
+
+declare type OverlayFunc = () => React.ReactNode;
+
+export interface UploadImageProps extends DropDownProps {
+  overlay: React.ReactNode | OverlayFunc;
+  placement?: 'bottomLeft' | 'bottomRight' | 'topLeft' | 'topCenter' | 'topRight' | 'bottomCenter';
+}
+
+export default class UploadImage extends React.Component<UploadImageProps, any> {}
